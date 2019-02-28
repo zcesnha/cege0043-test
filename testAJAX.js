7b07462b977980de0d97dab17d7eb962c6979e6c
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-02-28 13:35:45 -0500

    Working DIV with test.html File

--- testAJAX.js
+++ testAJAX.js
@@ -6,13 +6,12 @@ xhr.onreadystatechange = processDivChange;
 xhr.send();
 }
 function processDivChange() {
-if (xhr.readyState < 4) // while waiting response from
-server
+if (xhr.readyState < 4) // while waiting response from server
  document.getElementById('div1').innerHTML = "Loading...";
- else if (xhr.readyState === 4) { // 4 = Response from server has
-been completely loaded.
+ else if (xhr.readyState === 4) { // 4 = Response from server has been completely loaded.
  if (xhr.status == 200 && xhr.status < 300)
 // http status between 200 to 299 are all successful
  document.getElementById('div1').innerHTML = xhr.responseText;
  }
-}
+} 
+
