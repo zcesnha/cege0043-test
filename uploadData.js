68b9eecbd60a18cedd0595cd6cdd98631a9df287
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-03-01 08:56:11 -0500

    Addition of Data to Upload Function

--- uploadData.js
+++ uploadData.js
@@ -1,3 +1,9 @@
 function startDataUpload() {
-alert ("start data upload");
+alert ("start data upload"); 
+//Adds data to function-takes values from form and sends to database
+var name = document.getElementById("name").value;
+var surname = document.getElementById("surname").value;
+var module = document.getElementById("module").value;
+var postString = "name="+name +"&surname="+surname+"&module="+module;
+alert (postString);
 }
