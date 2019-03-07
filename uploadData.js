cf64e68a08b0e148a0b3b6495895fc7cca393753
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-03-07 08:43:38 -0500

    Data from buttons is now collected and sent to the server

--- uploadData.js
+++ uploadData.js
@@ -6,5 +6,29 @@ var surname = document.getElementById("surname").value;
 var module = document.getElementById("module").value;
 var postString = "name="+name +"&surname="+surname+"&module="+module;
 alert (postString); 
+
+//Takes data from the buttons in HTML form uploads them to the server- i.e should appear when "Start Data Upload" button is clicked 
+// now get the checkbox values - separate them with a | so that they can be
+// split later on if necessary
+var checkString = "";
+for (var i = 1;i< 5;i++){
+if (document.getElementById("check"+i).checked === true) {
+checkString = checkString +
+document.getElementById("check"+i).value + "||"
+}
+}
+postString = postString + "&modulelist="+checkString;
+// now get the radio button values
+if (document.getElementById("morning").checked) {
+postString=postString+"&lecturetime=morning";
+}
+if (document.getElementById("afternoon").checked) {
+postString=postString+"&lecturetime=afternoon";
+}
+// now get the select box values
+var language = document.getElementById("languageselectbox").value;
+postString = postString + "&language="+language;
+
+//should always be last in the method 
 processData (postString); 
 }
