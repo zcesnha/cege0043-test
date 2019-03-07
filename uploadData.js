9728b3481ad883db159d9f62b6216ad2ec6174a3
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-03-07 08:34:03 -0500

    Working AJAX Request-send data to the server

--- uploadData.js
+++ uploadData.js
@@ -5,5 +5,6 @@ var name = document.getElementById("name").value;
 var surname = document.getElementById("surname").value;
 var module = document.getElementById("module").value;
 var postString = "name="+name +"&surname="+surname+"&module="+module;
-alert (postString);
+alert (postString); 
+processData (postString); 
 }
